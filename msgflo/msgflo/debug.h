8f5417b779705909914b009c1c749e133cf75f6b
coon <coon@c-base.org>
2017-11-25 04:05:56 +0100

    plugin: only attach debug console in debug build

--- msgflo/msgflo/debug.h
+++ msgflo/msgflo/debug.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 
-#define dbg(...) printf(__VA_ARGS__)
-#define trace() dbg("%s()\n", __FUNCTION__)
+#ifdef _DEBUG
+  #define dbg(...) printf(__VA_ARGS__)
+  #define trace() dbg("%s()\n", __FUNCTION__)
+#else
+  #define dbg(...) __noop;
+  #define trace()  __noop;
+#endif
 
 #endif // _DEBUG_H
