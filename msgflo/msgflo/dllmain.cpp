6688fa5649a7606a7224d935ca3a8645eb6cd382
coon <coon@c-base.org>
2017-12-08 19:12:34 +0100

    dllmain.cpp: replace extern "C" definitions by one block

--- msgflo/msgflo/dllmain.cpp
+++ msgflo/msgflo/dllmain.cpp
@@ -22,42 +22,44 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
   return TRUE;
 }
 
-extern "C" __declspec(dllexport)
-void __cdecl setCallBacks(PluginInterfaceEntry uc) {
-  return MsgFlo::getInstance()->setCallBacks(uc);
-}
+extern "C" {
+  __declspec(dllexport)
+  void __cdecl setCallBacks(PluginInterfaceEntry uc) {
+    return MsgFlo::getInstance()->setCallBacks(uc);
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl onFirstCycle() {
-  return MsgFlo::getInstance()->onFirstCycle();
-}
+  __declspec(dllexport)
+  void __cdecl onFirstCycle() {
+    return MsgFlo::getInstance()->onFirstCycle();
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl onTick() {
-  return MsgFlo::getInstance()->onTick();
-}
+  __declspec(dllexport)
+  void __cdecl onTick() {
+    return MsgFlo::getInstance()->onTick();
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl onShutdown() {
-  return MsgFlo::getInstance()->onShutdown();
-}
+  __declspec(dllexport)
+  void __cdecl onShutdown() {
+    return MsgFlo::getInstance()->onShutdown();
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl buttonpress_event(UccncButton button, bool onScreen) {
-  MsgFlo::getInstance()->buttonPressEvent(button, onScreen);
-}
+  __declspec(dllexport)
+  void __cdecl buttonpress_event(UccncButton button, bool onScreen) {
+    MsgFlo::getInstance()->buttonPressEvent(button, onScreen);
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl textfieldclick_event(UccncField label, bool isMainScreen) {
-  MsgFlo::getInstance()->textFieldClickEvent(label, isMainScreen);
-}
+  __declspec(dllexport)
+  void __cdecl textfieldclick_event(UccncField label, bool isMainScreen) {
+    MsgFlo::getInstance()->textFieldClickEvent(label, isMainScreen);
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl textfieldtexttyped_event(UccncField label, bool isMainScreen, const char* pText) {
-  MsgFlo::getInstance()->textFieldTextTypedEvent(label, isMainScreen, pText);
-}
+  __declspec(dllexport)
+  void __cdecl textfieldtexttyped_event(UccncField label, bool isMainScreen, const char* pText) {
+    MsgFlo::getInstance()->textFieldTextTypedEvent(label, isMainScreen, pText);
+  }
 
-extern "C" __declspec(dllexport)
-void __cdecl getproperties_event(char* pAuthor, char* pPluginName, char* pPluginVersion) {
-  MsgFlo::getInstance()->getPropertiesEvent(pAuthor, pPluginName, pPluginVersion);
+  __declspec(dllexport)
+  void __cdecl getproperties_event(char* pAuthor, char* pPluginName, char* pPluginVersion) {
+    MsgFlo::getInstance()->getPropertiesEvent(pAuthor, pPluginName, pPluginVersion);
+  }
 }
