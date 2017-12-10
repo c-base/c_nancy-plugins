5eb6fe931888f6db5a51c310a081cf7eb1756dfc
coon <coon@c-base.org>
2017-12-10 15:25:55 +0100

    add midiplayer plugin

---
+++ msgflo/midiplayer/midiplayer.cpp
@@ -0,0 +1,67 @@
+#include "midiplayer.h"
+
+UccncPlugin* UccncPlugin::create() {
+  return new MidiPlayer();
+}
+
+MidiPlayer::MidiPlayer() : UccncPlugin(AUTHOR, PLUGIN_NAME, PLUGIN_VERSION) {
+  trace();
+
+}
+
+MidiPlayer::~MidiPlayer() {
+  trace();
+}
+
+void MidiPlayer::onFirstCycle() {
+  trace();
+}
+
+void MidiPlayer::onTick() {
+  // trace();
+
+}
+
+void MidiPlayer::onShutdown() {
+  trace();
+}
+
+void MidiPlayer::buttonPressEvent(UccncButton button, bool onScreen) {
+  trace();
+
+  if (onScreen) {
+    if (button == UccncButton::Cyclestart) {
+      // TODO: implement
+
+    }
+  }
+}
+
+void MidiPlayer::textFieldClickEvent(UccncField label, bool isMainScreen) {
+  const char* pLabel;
+
+  switch (label) {
+    case UccncField::Mdi:
+      pLabel = "Mdi";
+      break;
+
+    case UccncField::Setnextlinefield:
+      pLabel = "Setnextlinefield";
+      break;
+
+    default:
+      pLabel = "Unknown";
+  }
+
+  dbg("textFieldClickEvent; label=%s, isMainScreen=%d\n", pLabel, isMainScreen);
+}
+
+void MidiPlayer::textFieldTextTypedEvent(UccncField label, bool isMainScreen, const char* pText) {
+  trace();
+
+  if (isMainScreen) {
+    if (label == UccncField::Mdi) {
+      // TODO: implement
+    }
+  }
+}
---
+++ msgflo/midiplayer/midiplayer.h
@@ -0,0 +1,24 @@
+#ifndef _MIDIPLAYER_H
+#define _MIDIPLAYER_H
+
+#include "../msgflo/uccncplugin.h"
+#include "midiplayer.h"
+
+constexpr const char* AUTHOR =         "coon@c-base.org";
+constexpr const char* PLUGIN_NAME =    "midiplayer (c++)";
+constexpr const char* PLUGIN_VERSION = "10.12.17";
+
+class MidiPlayer : public UccncPlugin {
+public:
+  MidiPlayer();
+  ~MidiPlayer();
+
+  virtual void onFirstCycle() final override;
+  virtual void onTick() final override;
+  virtual void onShutdown() final override;
+  virtual void buttonPressEvent(UccncButton button, bool onScreen) final override;
+  virtual void textFieldClickEvent(UccncField label, bool isMainScreen) final override;
+  virtual void textFieldTextTypedEvent(UccncField label, bool isMainScreen, const char* pText) final override;
+};
+
+#endif // _MIDIPLAYER_H
