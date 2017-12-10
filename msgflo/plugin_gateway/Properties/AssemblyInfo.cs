b28fe5fbd80f509027f91190081e775e29f28eaa
coon <coon@c-base.org>
2017-12-10 11:53:30 +0100

    rename msgflo_gateway -> plugin_gateway

--- msgflo/plugin_gateway/Properties/AssemblyInfo.cs
+++ msgflo/plugin_gateway/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("plugin_gateway")]
-[assembly: AssemblyDescription("c++ binding plugin for UCCNC")]
+[assembly: AssemblyDescription("C++ binding plugin for UCCNC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("plugin_gateway")]
